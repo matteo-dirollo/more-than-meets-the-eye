2c604be9d800da9c890dc2f04ecade0d0dc26a6c
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2020-02-26 17:17:21 +0100

    New headline

--- config/website.js
+++ config/website.js
@@ -2,8 +2,8 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   title: 'MTMTE', // Navigation and Site Title
   titleAlt: 'More than meets the eye', // Title for JSONLD
-  description: 'A break into difficult topics',
-  headline: 'Writing and publishing content about new Media and Art', // Headline for schema.org JSONLD
+  description: 'A look beyond appearance, where the gap between visible and invisible becomes a place for the development of new languages.',
+  headline: 'Dive into the work of poeple who discovered the power of visual languages', // Headline for schema.org JSONLD
   url: 'https://mtmte.netlify.com/', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   logo: '/logos/logo-1024.png', // Used for SEO
@@ -12,7 +12,7 @@ module.exports = {
   // JSONLD / Manifest
   favicon: 'src/favicon.png', // Used for manifest favicon generation
   shortName: 'Prismic', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  author: 'Matteo Di Rollo', // Author for schemaORGJSONLD
   themeColor: '#3D63AE',
   backgroundColor: '#EBEDF2',
 
